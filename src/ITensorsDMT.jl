b49f9d6617acdaa6c9d6a8728ef52273d563f6bb
Stuart Thomas <snthomas@umd.edu>
2026-01-23 11:53:45 -0500

    change module name

--- src/ITensorsDMT.jl
+++ src/ITensorsDMT.jl
@@ -1,11 +1,13 @@
-module TEBD
+module ITensorsDMT
 
 using Random
 using ITensors
+using CUDA
 using LinearAlgebra
 abstract type TruncationMethod end
 
 
+# include("gpu.jl")
 include("basis.jl")
 include("utils.jl")
 include("dmt.jl")
